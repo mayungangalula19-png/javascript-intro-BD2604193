e9577c272ac1eff199d6a93ac9296dc7fcf1804f
mayungangalula19-png <mayungangalula19@gmail.com>
2026-04-09 12:05:47 +0300

    Add javaScript intr assignment

--- intro.js
+++ intro.js
@@ -1,1 +1,4 @@
-
+console.log("Name: Mayunga Ngalula");
+console.log("Age:20");
+console.log("Reason for learning the programming :I want to become a software developer and innovative system solutions.");
+console.log("Unique number:BD2604193");
